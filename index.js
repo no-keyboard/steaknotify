5d0af6e8f987d11a756ee4efc90a51f7866baaa8
no-keyboard <finaldestiny92@gmail.com>
2021-03-30 21:44:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const errorLog = process.env.ERROR_CHANNEL;
 const tagUser = process.env.TAG_USER;
 const url = process.env.URL;
 const urlList = url.split(";");
-const storesToCheck = ["Brooklyn, NY", "Long Island, NY"];
+const storesToCheck = ["Brooklyn, NY", "Long Island, NY", "New Haven, CT"];
 
 const createEmbed = (name, url, stores) => {
 	const inventoryFields = []
